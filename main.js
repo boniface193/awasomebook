a1701d9475286c0959d54ce7bb97d84f0f24e3f5
deboik193 <deboik193@gmail.com>
2023-03-21 20:29:10 +0100

    items save to localstorage

--- main.js
+++ main.js
@@ -5,23 +5,36 @@ const form = document.getElementById('form')
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
+  SubmitItem()
   form.reset()
+  ShowItem()
 })
 
-// const SubmitItem = () => {
-//   const payLoad = {
-//     title: collectItems[0].value,
-//     author: collectItems[1].value
-//   }
+function SubmitItem() {
+  const payLoad = {
+    title: collectItems[0].value,
+    author: collectItems[1].value
+  }
+  collectAllObj.push(payLoad)
+  localStorage.setItem('payLoad', JSON.stringify(collectAllObj))
+}
 
-//   collectAllObj.push(payLoad)
+function ShowItem() {
+  showItem.innerHTML = ''
+  const getpayLoad = JSON.parse(localStorage.getItem('payLoad'))
+  getpayLoad.forEach((item, index) => {
+    showItem.innerHTML += ` <aside>
+      <h3>${item.title}</h3>
+      <p>${item.author}</p>
+      <button onclick="removeItem(${index})">Remove</button>
+    </aside>
+    <hr>`
+  })
+} ShowItem()
 
-//   collectAllObj.forEach(item => {
-//     showItem.innerHtml += ` <aside>
-//       <h3>${item.title}</h3>
-//       <p>${item.author}</p>
-//       <button>Remove</button>
-//     </aside>
-//     <hr>`
-//   })
-// }
+function removeItem(params) {
+  const storage = JSON.parse(localStorage.getItem('payLoad'))
+  storage.splice(params, 1)
+  localStorage.setItem('payLoad', JSON.stringify(storage))
+  ShowItem() 
+}
